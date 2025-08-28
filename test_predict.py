c1423e7bd2d6eadc7e6070930ca6230ff73bf2a0
BishesnaShahi <bishesnas@gmail.com>
2025-08-28 10:18:43 +0545

    Normalize line endings to LF

--- test_predict.py
+++ test_predict.py
@@ -4,5 +4,5 @@ def test_positive():
     assert predict_sentiment("I love this movie!")[0] == 1
 
 def test_negative():
-    assert predict_sentiment("I hate this film!")[0] == 0
+    assert predict_sentiment("I dont like this film!")[0] == 0
 
